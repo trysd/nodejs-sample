d1e9e3bdc2cca51304e015989f380251c0d8a551
trysd <itrysd@icloud.com>
2023-01-04 18:15:15 +0900

    working on json map

---
+++ n02-file-creator/src/AnalyzeJson.ts
@@ -0,0 +1,139 @@
+import fs = require('fs-extra');
+
+/** Converted type */
+export interface ConvertedJson {
+  props: unknown,
+  system: {
+    file: string,
+    name: string,
+    path: string[],
+    pathStr: string
+  }
+}
+
+/** Class to parse map JSON */
+export class AnalyzeJson {
+
+  private static JsonURL = null;
+  constructor(jsonUrl: string) {
+    AnalyzeJson.JsonURL = jsonUrl;
+  }
+
+  /**
+   * JSON parsing main
+   */
+  public analyze(): ConvertedJson[] {
+
+    // read and parse
+    const json = this.readJson();
+
+    // Convert to an array representing what should be created where
+    const convertedJson: ConvertedJson[] = this.jsonToArray(json, [], []);
+
+    // Resolve annotation
+    this.readAnnotation(convertedJson);
+
+    return convertedJson;
+  }
+
+  /**
+   * Resolve annotations.
+   * the annotation value is converted to an array and replaced.
+   * @param arrRef whole array
+   * @param objRef Object to convert
+   * @param index Index to be converted from the entire array
+   * @param annoKey The key name of the annotation
+   */
+  public putAnnotation(
+    arrRef: ConvertedJson[],
+    objRef: ConvertedJson,
+    index: number,
+    annoKey: string
+  ): void {
+    const val = objRef.props[annoKey];
+    const annoFileType = val.replace(/^.*\@/, '');
+    const responseList: { name: string, path: string }[] = [];
+    const backCount = (val.match(/\.\.\//g) || []).length;
+    const targetPath = objRef.system.path.slice(0, objRef.system.path.length - backCount);
+    arrRef.forEach((cj, i) => {
+      if (i != index && targetPath.join("") === cj.system.path.join("") && annoFileType === cj.system.file) {
+        responseList.push({
+          name: cj.system.name,
+          path: '../'.repeat(backCount),
+        })
+      }
+    });
+    objRef.props[annoKey] = responseList;
+  }
+
+  /**
+   * Annotation analysis
+   * @param arrRef 
+   */
+  public readAnnotation(arrRef: ConvertedJson[]): void {
+    arrRef.forEach((obj, i) => {
+      Object.keys(obj.props).forEach(key => {
+        const val = obj.props[key];
+        if (val.match(/^(\.\.\/)*\@\w+/)) {
+          this.putAnnotation(arrRef, obj, i, key)
+        }
+      });
+    });
+  }
+
+  /**
+   * Convert Object to array with path
+   * @param json 
+   * @param arr 
+   * @param path 
+   * @returns 
+   */
+  public jsonToArray(json: unknown, arr: ConvertedJson[], path: string[]): ConvertedJson[] {
+    Object.keys(json).forEach(key => {
+      if (key.match(/\/$/)) {
+        // console.log(path, key)
+        const _path = [...path, key];
+        this.jsonToArray(json[key], arr, _path)
+      } else {
+        const keySplitted = key.split('=');
+        arr.push({
+          props: json[key],
+          system: {
+            file: keySplitted[0],
+            name: keySplitted[1],
+            path: path,
+            pathStr: path.join("")
+          }
+        })
+      }
+    });
+    return arr;
+  }
+
+  /**
+   * json read and parse with remove comment
+   * @returns 
+   */
+  public readJson(): unknown {
+    const jsonStr = fs.readFileSync(AnalyzeJson.JsonURL).toString();
+    const json = JSON.parse(jsonStr);
+    this.removeJsonCommentOut(json);
+    return json;
+  }
+
+  /**
+   * Remove the // at the beginning of the item name.
+   * The / at the end of the item name is a recursive search.
+   * @param json json of map
+   */
+  public removeJsonCommentOut(json: unknown): void {
+    Object.keys(json).forEach(key => {
+      if (key.match(/^\/\//)) {
+        delete json[key]
+      } else if (key.match(/\/$/)) {
+        this.removeJsonCommentOut(json[key])
+      }
+    });
+  }
+
+}
--- n02-file-creator/src/index.ts
+++ n02-file-creator/src/index.ts
@@ -2,71 +2,111 @@ import path = require('path');
 import fs = require('fs-extra');
 import ejs = require('ejs');
 
-export default class Main {
+import { AnalyzeJson } from './AnalyzeJson';
 
+// とりあえず自分で使い勝手がいいものを作る
+
+// 後は配列に従ってファイルを作成するだけ
+// 順序
+// ・テンプレート事前準備
+// ・ループ
+// ・ejs置換
+// ・フォルダ作成しつつファイル出力
+// 
+
+// -> 出力済みのファイルの対応
+//    -> 基本的にはエラー。-f オプション付与を促す
+//    -> 強制上書きオプション？（消去してから出力）
+
+// -> マップの逆書き出し(後足し表記?) ※例: { newCompo: "__afterDetect__" }
+// -> (\w+).stories.tsx なら、stories=$1 をmapに書き出す
+//    未知のファイルの場合、unknow={ファイル名そのまま} をmapに書き出す
+//    但し、これらのファイルは -f をつけた実行でも絶対に強制的出力しない。
+
+// jsonエラーチェック
+// ・末尾/のない階層の配下に末尾/付きの出現 => エラー
+// ・末尾/配下に、「末尾/」or「=付き」項目名ではない => エラー
+
+// コマンド
+// 実行 npm run 
+
+/** .. */
+export default class Main {
   private static tplDir = 'x.files';
 
-  /*
+  json = new AnalyzeJson("./x.files.map.json");
 
-    ・引数ありの場合
-      引数でpath, props決定
-    ・引数なしの場合
-      json設定ファイルでpath, props決定
+  base = path.resolve();
 
-  */
-  /**
-   * main
-   */
+  /** main */
   public main(): void {
-    console.log("main!");
-    this.typeArgv();
+
+    // Convert to an array representing what should be created where
+    const convertedJson = this.json.analyze();
+
+    // ..
+    console.dir(convertedJson, { depth: null })
+    console.log("path: " + this.base);
+
+
   }
 
-  private typeArgv(): void {
+  // ===========
+  // =========== OLD
+  // ===========
+
+  // // 引数があれば - 引数でpath, props決定
+  // this.typeArgv({
+  //   name: "AAA",
+  //   comment: "sss"
+  // });
+
+  /**  */
+  private typeArgv(props): void {
 
     const base = path.resolve();
 
-    // const targetPath = base + '/src/aaa/bbb/'
-    // console.log(targetPath);
+    // // tplフォルダ解析
+    // const tplPathStr = this.readDir(base + '/' + Main.tplDir);
+    // console.log(tplPathStr);
 
+    // tplPathStr.forEach(f => {
+    //   const tplPath = base + '/' + Main.tplDir + '/' + f;
+    //   const str = fs.readFileSync(tplPath).toString();
+    //   const strCompiled = ejs.render(str, props).replace(/\n\n+/g, '\n\n');
+    //   console.log('---------- ' + f)
+    // });
+
+    // console.log(process.argv);
+    // tplBodyList.forEach(..
     // fs.mkdirs(targetPath, function (err) {
     //   if (err) return console.error(err)
     //   console.log("success!")
     // });
-
+    // const targetPath = base + '/src/aaa/bbb/'
+    // console.log(targetPath);
     // fs.outputFileSync(targetPath + '/test.txt', 'hello!');
-
-    // ファイル読み込んで
-    const tplPathStr = this.readDir(base + '/' + Main.tplDir);
-    // console.log(tplPathStr);
-    const tplBodes = tplPathStr.map(f => {
-      console.log(f);
-      const tplPath = base + '/' + Main.tplDir + '/' + f;
-      const str = fs.readFileSync(tplPath).toString();
-      const strCompiled = ejs.render(str, {
-        name: "testABC",
-        path: '',
-        components: '',
-        jsx: '',
-        states: '',
-        props: ''
-      }).replace(/\n\n+/g, '\n\n');
-
-      console.log('-----');
-      console.log(strCompiled);
-      console.log('-----//');
-    });
-    console.log(tplBodes)
   }
 
+
+
+
   /**
-   * ディレクトリ内ファイル一覧取得
-   * @param dirPath 
+   * Get recursive directory structure
+   * @param dirPath
+   * @param res response value
+   * @param route stack structure
   */
-  public readDir(dirPath: string): string[] {
-    const res: string[] = [];
-    const dirent = fs.readdirSync(dirPath, { withFileTypes: true });
-    dirent.forEach(d => res.push(d.name))
+  public readDir(dirPath: string, res: string[] = [], route: string[] = []): string[] {
+    const routesStr = (route.length ? route.join('/') + '/' : '');
+    const dirent = fs.readdirSync(dirPath + '/' + routesStr, { withFileTypes: true });
+    dirent.forEach(d => {
+      if (d.isDirectory()) {
+        res = [...res, ...(this.readDir(dirPath, res, [...route, d.name]))];
+      } else {
+        res.push(routesStr + d.name)
+      }
+    })
     return res;
   }
 
